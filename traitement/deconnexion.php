70fbbc3a11eaf2df72c60b05173ec952da1978d6
Antoine <antoine@meresse.info>
2016-10-11 12:33:24 +0200

    deconnexion

--- traitement/deconnexion.php
+++ traitement/deconnexion.php
@@ -1,3 +1,10 @@
 <?php
 // Celui la aussi ressemble au TP4
+session_start();
+
+if(isset($_SESSION["login"]) && isset($_SESSION["id"]))
+{
+	unset($_SESSION["login"]);
+	unset($_SESSION["id"]);
+}
 ?>
