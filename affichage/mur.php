e89bc4564c3793bcd98c7d00a18a6608061abcfa
Antoine <antoine@meresse.info>
2016-10-12 10:17:37 +0200

    mur (Fait par Pierre)

--- affichage/mur.php
+++ affichage/mur.php
@@ -10,7 +10,15 @@ if(!isset($_SESSION['id'])) {
 
 
 include("entete.php");
+	
 
+	
+	$sql="SELECT * FROM utilisateur where id=?";
+	$who = $pdo->prepare ($sql);
+	$who -> execute(array($_GET['id']));
+	$line = $who -> fetch();
+	echo "Vous êtes sur le mur de ".$line['login'];
+	
 if(!isset($_GET['id']) || !is_numeric($_GET['id'])) {
 	// On n a pas donné le numéro de l'id de la personne dont on veut afficher le mur.
 	// On affiche un message et on meurt
@@ -26,31 +34,71 @@ if($_GET['id']==$_SESSION['id']) {
 	// Verifions si on est amis avec cette personne
 	$sql = "SELECT * FROM lien WHERE etat='ami' 
 		AND ((idUtilisateur1=? AND idUtilisateur2=?) OR ((idUtilisateur1=? AND idUtilisateur2=?)))";
-
+	$ami = $pdo->prepare($sql);
+	$ami -> execute(array($_GET['id'],$_SESSION['id'],$_SESSION['id'],$_GET['id']));
+	$line= $ami -> fetch();
+	if ($line!==false) {
+	$ok = true;
+	}	
+	 
+	
 	// les deux ids à tester sont : $_GET['id'] et $_SESSION['id']		
 	// A completer. Il faut récupérer une ligne, si il y en a pas ca veut dire que l'on est pas ami avec cette personne
 	
 
-}
+	
+
+ }
 if($ok==false) {
-	echo "Vous n'êtes aps encore ami, vous ne pouvez voir son mur !!";
-	die(1);
-}
-
-
-
+	echo "<br/> Vous n'êtes pas encore ami, vous ne pouvez voir son mur !!<br/> ".lien('../traitement/demanderamitie.php?id='.$_GET['id'],'Demander en ami');
+} else {
 
 // Tout va bien, il maintenant possible d'afficher le mur en question.
 // A completer
 // Requête de sélection des éléments d'un mur
-// SELECT * FROM ecrit WHERE idAmi=?
 // le paramètre  est le $_GET['id']
+
+ $sql="SELECT * FROM ecrit WHERE idAmi=?";
+ $querry = $pdo->prepare ($sql);
+ $querry -> execute(array($_GET['id']));
+ 
+ while ($line = $querry -> fetch()){
+	echo "Titre: ";
+	echo $line['titre']."<br/>";
+	echo "Message: ";
+	echo $line['contenu']."<br/>";
+ 
+	$sql="SELECT * FROM utilisateur where id=?";
+	$q = $pdo->prepare ($sql);
+	$q -> execute(array($line['idAuteur']));
+	 $line2 = $q -> fetch();
+		echo 'Ecrit par';
+	echo lien('mur.php?id='.$line['idAuteur'],$line2['login']);	
+		echo " le ";
+	echo $line['dateEcrit'];
+	if($_SESSION['id'] == $_GET['id'] || $_SESSION['id'] == $line['idAuteur']){
+	echo lien("../traitement/effacer.php?id=".$line['id'],"Supprimer");
+		echo "<br/>" ;
+	}
+	
+}
+
+
 
 
 
 ?>
 
+<form action="../traitement/ecrire.php" method="POST">
+	<input type="text" name="titre"><br>
+	<input type="hidden" name="idAmi" value="<?php echo $_GET['id'];?>"><br>	
+	<textarea placeholder="Lache ton message Bro" name="contenu" ></textarea><br/>
+	<input type="submit" name="Envoyer" value="Envoyer">
+	
+</form>	
+
 <?php
+} // else pas ami 
 // On termine par le pied de page
 
 include("pied.php");
