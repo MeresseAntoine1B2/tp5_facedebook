73ba7789603e36992dc1ee97b57f788df5142f62
Antoine <antoine@meresse.info>
2016-10-19 11:01:32 +0200

    ecrire -> OK mais sans images)

--- traitement/ecrire.php
+++ traitement/ecrire.php
@@ -9,8 +9,11 @@ if(!isset($_SESSION['id'])) {
 	header("Location:login.php");
 }
 
-
+$date = date('Y-m-d H:i:s');
 // Ecrire un message
+$sql = "INSERT INTO ecrit VALUES (NULL, ?, ?, ?, ?, ?, ?)";
+$q = $pdo->prepare($sql);
+$q->execute(array($_POST["titre"], $_POST["contenu"], $date, 'NULL', $_SESSION["id"], $_POST["idAmi"]));
 
-
+header("Location:".$_SERVER['HTTP_REFERER']);
 ?>
--- traitement/valideramitie.php
+++ traitement/valideramitie.php
@@ -8,10 +8,7 @@ if(!isset($_SESSION['id'])) {
 	// On n'est pas connecté, il faut retourner à la pgae de login
 	header("Location:login.php");
 }
-else 
-{
-	
-}
+
 // La requete de demande de creation amitie : UPDATE lien set etat=? INTO lien WHERE idUtilisateur1=? and idUtilisateur2=?
 // Le premier paramètre de la requête la valeur banni si on veut pas ou la valeur ami si on est d'accord
 // Le second paramètre de la requête est le $_POST['id'] : celui a qui on repond
